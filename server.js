db4287bacf8d4a85762b54be3ceeebb5e1cc175d
marat-landed <mrtland7@gmail.com>
2018-04-06 16:26:01 +0300

    06-04-18 16:25

--- server.js
+++ server.js
@@ -3,22 +3,12 @@ var express = require('express'),
     app = express();
 //    eps = require('ejs'),
 //    morgan = require('morgan');
-
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = express();
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-
-
 // app is running!
 app.get('/', function(req, res) {
     res.send('Hello from NodeJS  at '+ new Date() + '. Edit 06-04-18 16:14');
 });
-
-
-
-app.listen(8080, ip);
-
-
-
+app.listen(port, ip);
 module.exports = app;
